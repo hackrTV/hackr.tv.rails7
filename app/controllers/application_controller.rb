ad15905c30b7d07ce0c9ed2ca3f152329e652bb8
XERAEN <x@XERAEN.net>
2024-11-23 15:55:22 -0600

    Adds favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,30 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  #allow_browser versions: :modern
+
+  before_action :set_browser
 
   def index
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+    puts "====================================================================="
+
+    puts ""
+    puts @browser.device.mobile?
+    puts ""
+
+    puts "====================================================================="
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+  end
+
+  private
+
+  def set_browser
+    @browser = Browser.new(request.env["HTTP_USER_AGENT"])
   end
 end
