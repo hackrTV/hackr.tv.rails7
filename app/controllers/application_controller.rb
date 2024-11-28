4590f09a234bfd31153041fde50b38df46abf6a6
XERAEN <x@XERAEN.net>
2024-11-28 15:52:26 -0600

    Removes unused browser gem code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  #allow_browser versions: :modern
-
-  before_action :set_browser
+  allow_browser versions: :modern
 
   def index
-    puts ""
-    puts ""
-    puts ""
-    puts ""
-    puts "====================================================================="
-
-    puts ""
-    puts @browser.device.mobile?
-    puts ""
-
-    puts "====================================================================="
-    puts ""
-    puts ""
-    puts ""
-    puts ""
-  end
-
-  private
-
-  def set_browser
-    @browser = Browser.new(request.env["HTTP_USER_AGENT"])
   end
 end
